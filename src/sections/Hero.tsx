97507557267f99513dde43cbd28c71176826998b
nikolamirilo <nikolamirilo@gmail.com>
2023-03-13 15:09:40 +0100

    added blur image

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,11 +4,16 @@ import Link from "next/link";
 import React from "react";
 
 const Hero: React.FC<HeroProps> = ({ title, subtitle, image }) => {
-  const blurDataURL =
-    "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMTI4IDEyOCIgd2lkdGg9IjEyOCIgaGVpZ2h0PSIxMjgiIHZlcnNpb249IjEuMSI+PHBhdGggZD0iTTAgMGgxMjh2MTI4SDB6IiBmaWxsPSIjMDAxZjNmIiAvPjwvc3ZnPg==";
   return (
     <section id="hero">
-      <Image src={image} alt="Office" fill placeholder="blur" blurDataURL={blurDataURL} priority />
+      <Image
+        src={image}
+        alt="Office"
+        fill
+        placeholder="blur"
+        blurDataURL="/images/office-modified.webp"
+        priority
+      />
       <div className="hero-content">
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
