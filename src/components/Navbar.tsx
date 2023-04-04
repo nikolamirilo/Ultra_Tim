3512a0e1dec9af2c40a7dd5849b994eceec84ba4
nikolamirilo <nikolamirilo@gmail.com>
2023-04-04 15:11:32 +0200

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { i18n, useTranslation } from "next-i18next";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import EN from "../../public/images/EN.webp";
 import logo from "../../public/images/logo.webp";
 import SR from "../../public/images/SR.webp";
@@ -10,6 +10,7 @@ import SR from "../../public/images/SR.webp";
 const Navbar: React.FC = () => {
   const router = useRouter();
   const { route, asPath, query } = router;
+  const [isOpen, setIsOpen] = useState(false);
   const { t } = useTranslation();
   async function handleChangeLanguage(lang: string) {
     if (i18n) {
@@ -18,19 +19,50 @@ const Navbar: React.FC = () => {
       window.location.reload();
     }
   }
+  console.log(isOpen);
   return (
     <nav className="navbar">
       <div className="logo">
         <Image src={logo} alt="Logo" style={{ margin: "0.5vh 0" }} priority />
       </div>
-      <input id="menu-toggle" type="checkbox" />
-      <label className="menu-button-container" htmlFor="menu-toggle">
+      <label
+        className="menu-button-container"
+        id={isOpen ? "open" : "closed"}
+        onClick={() => {
+          if (isOpen) {
+            setIsOpen(false);
+          } else {
+            setIsOpen(true);
+          }
+        }}
+      >
         <div className="menu-button"></div>
       </label>
       <div className="links">
-        <Link href="/">{t("Početna")}</Link>
-        <Link href="/offer">{t("Ponuda")}</Link>
-        <Link href="/contact">{t("Kontakt")}</Link>
+        <Link
+          href="/"
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          {t("Početna")}
+        </Link>
+        <Link
+          href="/offer"
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          {t("Ponuda")}
+        </Link>
+        <Link
+          href="/contact"
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          {t("Kontakt")}
+        </Link>
         <button
           onClick={() => {
             handleChangeLanguage("sr");
