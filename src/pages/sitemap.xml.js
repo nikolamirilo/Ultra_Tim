2c95c699b0dc9aae8dc0d2f00810615ae05587f5
Nikola Mirilo <nikolamirilo@gmail.com>
2023-04-23 19:48:10 +0200

    updated sitemap.xml v4

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,24 +1,21 @@
-import moment from "moment";
-const date = moment().format("MMMM Do YYYY, h:mm:ss a");
-
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>https://ultra-tim-s.com/</loc>
-       <lastmod>${date}</lastmod>
+       <lastmod>2023-04-23T12:34:56-07:00</lastmod>
        <changefreq>weekly</changefreq>
        <priority>1</priority>
      </url>
      <url>
        <loc>https://ultra-tim-s.com/offer</loc>
-       <lastmod>${date}</lastmod>
+       <lastmod>2023-04-23T12:34:56-07:00</lastmod>
        <changefreq>weekly</changefreq>
        <priority>0.9</priority>
      </url>
      <url>
        <loc>https://ultra-tim-s.com/contact</loc>
-       <lastmod>${date}</lastmod>
+       <lastmod>2023-04-23T12:34:56-07:00</lastmod>
        <changefreq>weekly</changefreq>
        <priority>0.8</priority>
      </url>
