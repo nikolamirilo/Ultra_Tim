086739a84fd2cc39ddb70a391b54023ed686d2bf
Nikola Mirilo <nikolamirilo@gmail.com>
2023-04-23 19:36:18 +0200

    upadted sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -5,7 +5,7 @@ function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
-       <loc>https://ultra-tim.com/</loc>
+       <loc>https://ultra-tim-s.com/</loc>
        <lastmod>${date}</lastmod>
        <changefreq>weekly</changefreq>
        <priority>1</priority>
